394f20a090f4b95f05cca198add653969274acf4
Shuxian Wang <shuxian.wang@antithesis.com>
2024-11-17 00:01:42 +0800

    Add rich assertions
    - Supports for both numeric & boolean guidance types are added, with only the numeric guidance providing built-in filtering.
    - Similar to assertions, guidances should also be cataloged by the instrumentor, arranging `registerGuidance` calls at program start up time.

---
+++ src/assert/guidance.ts
@@ -0,0 +1,92 @@
+import * as internal from '../internal'
+import { locationToJSON, type LocationInfo } from './location'
+import { Tracker } from './tracker'
+
+class NumericGuard {
+    #mark: number
+    #maximize: boolean
+
+    constructor(maximize: boolean) {
+        this.#maximize = maximize
+        this.#mark = maximize
+            ? Number.NEGATIVE_INFINITY
+            : Number.POSITIVE_INFINITY
+    }
+
+    shouldEmit(value: number) {
+        if (this.#maximize ? this.#mark > value : this.#mark < value)
+            return false
+
+        // Report NaN values, but don't let them update the mark.
+        if (!Number.isNaN(value)) {
+            this.#mark = value
+        }
+        return true
+    }
+}
+
+const NUMERIC_GUARDS: Tracker<string, NumericGuard> = new Tracker()
+
+export type GuidanceData = {
+    numeric: { left: number; right: number }
+    boolean: { [key: string]: boolean }
+}
+
+export type GuidanceType = keyof GuidanceData
+
+export const registerGuidance = ({
+    id,
+    message,
+    location,
+    guidanceType,
+    maximize,
+}: {
+    id: string
+    message: string
+    location: LocationInfo
+    guidanceType: GuidanceType
+    maximize: boolean
+}) => {
+    internal.output({
+        antithesis_guidance: {
+            id,
+            message,
+            location: locationToJSON(location),
+            guidance_type: guidanceType,
+            maximize,
+            hit: false,
+            guidance_data: null,
+        },
+    })
+}
+
+export const hitGuidance = <T extends GuidanceType>(
+    args: T extends unknown
+        ? {
+              id: string
+              message: string
+              location: LocationInfo
+              guidanceType: T
+              maximize: boolean
+              data: GuidanceData[T]
+          }
+        : never
+) => {
+    const { id, message, location, guidanceType, maximize, data } = args
+    if (guidanceType === 'numeric') {
+        const guard = NUMERIC_GUARDS.getOr(id, () => new NumericGuard(maximize))
+        if (!guard.shouldEmit(data.left - data.right)) return
+    }
+
+    internal.output({
+        antithesis_guidance: {
+            id,
+            message,
+            location: locationToJSON(location),
+            guidance_type: guidanceType,
+            maximize,
+            hit: true,
+            guidance_data: data,
+        },
+    })
+}
--- src/assert/index.ts
+++ src/assert/index.ts
@@ -1,9 +1,11 @@
 import { JSONObject } from '../internal'
 import { AssertType, LOCATION_TRACKER, hitAssertion } from './assert'
+import { type GuidanceData, type GuidanceType, hitGuidance } from './guidance'
 import { locationAtCallStack } from './location'
 
 export { JSONObject, AssertType, hitAssertion }
 export { registerAssertion } from './assert'
+export { registerGuidance } from './guidance'
 
 /**
  * Assert that condition is true every time this function is called, AND that
@@ -139,6 +141,408 @@ export function reachable(message: string, details?: JSONObject) {
     })
 }
 
+/**
+ * <code>alwaysGreaterThan(x, y, ...)</code> is mostly equivalent to
+ * <code>always(x > y, ...)</code>. Additionally Antithesis has more visibility
+ * to the value of <code>x</code> and <code>y</code>, and the assertion details
+ * would be merged with <code>{"left": x, "right": y}</code>.
+ *
+ * @param {number} left - The left-hand-side of the comparison.
+ *
+ * @param {number} right - The right-hand-side of the comparison.
+ *
+ * @param {string} message - A unique string identifier of the assertion.
+ * Provides context for assertion success/failure and is intended to be
+ * human-readable. Must be provided as a string literal.
+ *
+ * @param {JSONObject} details - Additional details that provide greater context
+ * for assertion success/failure. Evaluated at runtime.
+ *
+ * @see always
+ */
+export function alwaysGreaterThan(
+    left: number,
+    right: number,
+    message: string,
+    details?: JSONObject
+) {
+    assertImpl({
+        message,
+        assertType: 'always',
+        mustHit: true,
+        condition: left > right,
+        details: { left, right, ...details },
+    })
+    guidanceImpl({
+        message,
+        guidanceType: 'numeric',
+        maximize: false,
+        data: { left, right },
+    })
+}
+
+/**
+ * <code>alwaysGreaterThanOrEqualTo(x, y, ...)</code> is mostly equivalent to
+ * <code>always(x >= y, ...)</code>. Additionally Antithesis has more visibility
+ * to the value of <code>x</code> and <code>y</code>, and the assertion details
+ * would be merged with <code>{"left": x, "right": y}</code>.
+ *
+ * @param {number} left - The left-hand-side of the comparison.
+ *
+ * @param {number} right - The right-hand-side of the comparison.
+ *
+ * @param {string} message - A unique string identifier of the assertion.
+ * Provides context for assertion success/failure and is intended to be
+ * human-readable. Must be provided as a string literal.
+ *
+ * @param {JSONObject} details - Additional details that provide greater context
+ * for assertion success/failure. Evaluated at runtime.
+ *
+ * @see always
+ */
+export function alwaysGreaterThanOrEqualTo(
+    left: number,
+    right: number,
+    message: string,
+    details?: JSONObject
+) {
+    assertImpl({
+        message,
+        assertType: 'always',
+        mustHit: true,
+        condition: left >= right,
+        details: { left, right, ...details },
+    })
+    guidanceImpl({
+        message,
+        guidanceType: 'numeric',
+        maximize: false,
+        data: { left, right },
+    })
+}
+
+/**
+ * <code>alwaysLessThan(x, y, ...)</code> is mostly equivalent to <code>always(x
+ * < y, ...)</code>. Additionally Antithesis has more visibility to the value of
+ * <code>x</code> and <code>y</code>, and the assertion details would be merged
+ * with <code>{"left": x, "right": y}</code>.
+ *
+ * @param {number} left - The left-hand-side of the comparison.
+ *
+ * @param {number} right - The right-hand-side of the comparison.
+ *
+ * @param {string} message - A unique string identifier of the assertion.
+ * Provides context for assertion success/failure and is intended to be
+ * human-readable. Must be provided as a string literal.
+ *
+ * @param {JSONObject} details - Additional details that provide greater context
+ * for assertion success/failure. Evaluated at runtime.
+ *
+ * @see always
+ */
+export function alwaysLessThan(
+    left: number,
+    right: number,
+    message: string,
+    details?: JSONObject
+) {
+    assertImpl({
+        message,
+        assertType: 'always',
+        mustHit: true,
+        condition: left < right,
+        details: { left, right, ...details },
+    })
+    guidanceImpl({
+        message,
+        guidanceType: 'numeric',
+        maximize: true,
+        data: { left, right },
+    })
+}
+
+/**
+ * <code>alwaysLessThanOrEqualTo(x, y, ...)</code> is mostly equivalent to
+ * <code>always(x <= y, ...)</code>. Additionally Antithesis has more visibility
+ * to the value of <code>x</code> and <code>y</code>, and the assertion details
+ * would be merged with <code>{"left": x, "right": y}</code>.
+ *
+ * @param {number} left - The left-hand-side of the comparison.
+ *
+ * @param {number} right - The right-hand-side of the comparison.
+ *
+ * @param {string} message - A unique string identifier of the assertion.
+ * Provides context for assertion success/failure and is intended to be
+ * human-readable. Must be provided as a string literal.
+ *
+ * @param {JSONObject} details - Additional details that provide greater context
+ * for assertion success/failure. Evaluated at runtime.
+ *
+ * @see always
+ */
+export function alwaysLessThanOrEqualTo(
+    left: number,
+    right: number,
+    message: string,
+    details?: JSONObject
+) {
+    assertImpl({
+        message,
+        assertType: 'always',
+        mustHit: true,
+        condition: left <= right,
+        details: { left, right, ...details },
+    })
+    guidanceImpl({
+        message,
+        guidanceType: 'numeric',
+        maximize: true,
+        data: { left, right },
+    })
+}
+
+/**
+ * <code>sometimesGreaterThan(x, y, ...)</code> is mostly equivalent to
+ * <code>sometimes(x > y, ...)</code>. Additionally Antithesis has more
+ * visibility to the value of <code>x</code> and <code>y</code>, and the
+ * assertion details would be merged with <code>{"left": x, "right": y}</code>.
+ *
+ * @param {number} left - The left-hand-side of the comparison.
+ *
+ * @param {number} right - The right-hand-side of the comparison.
+ *
+ * @param {string} message - A unique string identifier of the assertion.
+ * Provides context for assertion success/failure and is intended to be
+ * human-readable. Must be provided as a string literal.
+ *
+ * @param {JSONObject} details - Additional details that provide greater context
+ * for assertion success/failure. Evaluated at runtime.
+ *
+ * @see sometimes
+ */
+export function sometimesGreaterThan(
+    left: number,
+    right: number,
+    message: string,
+    details?: JSONObject
+) {
+    assertImpl({
+        message,
+        assertType: 'sometimes',
+        mustHit: true,
+        condition: left > right,
+        details: { left, right, ...details },
+    })
+    guidanceImpl({
+        message,
+        guidanceType: 'numeric',
+        maximize: true,
+        data: { left, right },
+    })
+}
+
+/**
+ * <code>sometimesGreaterThanOrEqualTo(x, y, ...)</code> is mostly equivalent
+ * to <code>sometimes(x >= y, ...)</code>. Additionally Antithesis has more
+ * visibility to the value of <code>x</code> and <code>y</code>, and the
+ * assertion details would be merged with <code>{"left": x, "right": y}</code>.
+ *
+ * @param {number} left - The left-hand-side of the comparison.
+ *
+ * @param {number} right - The right-hand-side of the comparison.
+ *
+ * @param {string} message - A unique string identifier of the assertion.
+ * Provides context for assertion success/failure and is intended to be
+ * human-readable. Must be provided as a string literal.
+ *
+ * @param {JSONObject} details - Additional details that provide greater context
+ * for assertion success/failure. Evaluated at runtime.
+ *
+ * @see sometimes
+ */
+export function sometimesGreaterThanOrEqualTo(
+    left: number,
+    right: number,
+    message: string,
+    details?: JSONObject
+) {
+    assertImpl({
+        message,
+        assertType: 'sometimes',
+        mustHit: true,
+        condition: left >= right,
+        details: { left, right, ...details },
+    })
+    guidanceImpl({
+        message,
+        guidanceType: 'numeric',
+        maximize: true,
+        data: { left, right },
+    })
+}
+
+/**
+ * <code>sometimesLessThan(x, y, ...)</code> is mostly equivalent to <code>sometimes(x < y, ...)</code>.
+ * Additionally Antithesis has more visibility to the value of <code>x</code> and <code>y</code>,
+ * and the assertion details would be merged with <code>{"left": x, "right": y}</code>.
+ *
+ * @param {number} left - The left-hand-side of the comparison.
+ *
+ * @param {number} right - The right-hand-side of the comparison.
+ *
+ * @param {string} message - A unique string identifier of the assertion.
+ * Provides context for assertion success/failure and is intended to be
+ * human-readable. Must be provided as a string literal.
+ *
+ * @param {JSONObject} details - Additional details that provide greater context
+ * for assertion success/failure. Evaluated at runtime.
+ *
+ * @see sometimes
+ */
+export function sometimesLessThan(
+    left: number,
+    right: number,
+    message: string,
+    details?: JSONObject
+) {
+    assertImpl({
+        message,
+        assertType: 'sometimes',
+        mustHit: true,
+        condition: left < right,
+        details: { left, right, ...details },
+    })
+    guidanceImpl({
+        message,
+        guidanceType: 'numeric',
+        maximize: false,
+        data: { left, right },
+    })
+}
+
+/**
+ * <code>sometimesLessThanOrEqualTo(x, y, ...)</code> is mostly equivalent to <code>sometimes(x <= y, ...)</code>.
+ * Additionally Antithesis has more visibility to the value of <code>x</code> and <code>y</code>,
+ * and the assertion details would be merged with <code>{"left": x, "right": y}</code>.
+ *
+ * @param {number} left - The left-hand-side of the comparison.
+ *
+ * @param {number} right - The right-hand-side of the comparison.
+ *
+ * @param {string} message - A unique string identifier of the assertion.
+ * Provides context for assertion success/failure and is intended to be
+ * human-readable. Must be provided as a string literal.
+ *
+ * @param {JSONObject} details - Additional details that provide greater context
+ * for assertion success/failure. Evaluated at runtime.
+ *
+ * @see sometimes
+ */
+export function sometimesLessThanOrEqualTo(
+    left: number,
+    right: number,
+    message: string,
+    details?: JSONObject
+) {
+    assertImpl({
+        message,
+        assertType: 'sometimes',
+        mustHit: true,
+        condition: left <= right,
+        details: { left, right, ...details },
+    })
+    guidanceImpl({
+        message,
+        guidanceType: 'numeric',
+        maximize: false,
+        data: { left, right },
+    })
+}
+
+/**
+ * <code> alwaysSome({a: x, b: y, ...}, ...)</code> is similar to <code>always(x
+ * || y || ..., ...)</code>. Additionally:
+ * <ul>
+ *   <li>Antithesis has more visibility to the individual propositions.</li>
+ *   <li>There is no short-circuiting, so all of <code>x</code>, <code>y</code>, ... would be evaluated.</li>
+ *   <li>The assertion details would be merged with <code>{"a": x, "b": y, ...}</code>.</li>
+ * </ul>
+ *
+ * @param {Object.<string, boolean>} conditions - The collection of conditions
+ * to-be disjuncted, represented as a map of booleans indexed by strings.
+ *
+ * @param {string} message - A unique string identifier of the assertion.
+ * Provides context for assertion success/failure and is intended to be
+ * human-readable. Must be provided as a string literal.
+ *
+ * @param {JSONObject} details - Additional details that provide greater context
+ * for assertion success/failure. Evaluated at runtime.
+ *
+ * @see always
+ */
+export function alwaysSome(
+    conditions: { [key: string]: boolean },
+    message: string,
+    details?: JSONObject
+) {
+    const condition = Object.values(conditions).some((c) => c)
+    assertImpl({
+        message,
+        assertType: 'always',
+        mustHit: true,
+        condition,
+        details: { ...conditions, ...details },
+    })
+    guidanceImpl({
+        message,
+        guidanceType: 'boolean',
+        maximize: false,
+        data: conditions,
+    })
+}
+
+/**
+ * <code>sometimesAll({a: x, b: y, ...}, ...)</code> is similar to
+ * <code>sometimes(x && y && ..., ...)</code>. Additionally:
+ * <ul>
+ *   <li>Antithesis has more visibility to the individual propositions.</li>
+ *   <li>There is no short-circuiting, so all of <code>x</code>, <code>y</code>, ... would be evaluated.</li>
+ *   <li>The assertion details would be merged with <code>{"a": x, "b": y, ...}</code>.</li>
+ * </ul>
+ *
+ * @param {Object.<string, boolean>} conditions - The collection of conditions to-be disjuncted,
+ * represented as a map of booleans indexed by strings.
+ *
+ * @param {string} message - A unique string identifier of the assertion.
+ * Provides context for assertion success/failure and is intended to be human-readable.
+ * Must be provided as a string literal.
+ *
+ * @param {JSONObject} details - Additional details that provide greater context for assertion success/failure.
+ * Evaluated at runtime.
+ *
+ * @see sometimes
+ */
+export function sometimesAll(
+    conditions: { [key: string]: boolean },
+    message: string,
+    details?: JSONObject
+) {
+    const condition = Object.values(conditions).every((c) => c)
+    assertImpl({
+        message,
+        assertType: 'sometimes',
+        mustHit: true,
+        condition,
+        details: { ...conditions, ...details },
+    })
+    guidanceImpl({
+        message,
+        guidanceType: 'boolean',
+        maximize: true,
+        data: conditions,
+    })
+}
+
 function assertImpl(args: {
     message: string
     assertType: AssertType
@@ -149,4 +553,19 @@ function assertImpl(args: {
     const id = args.message
     const location = LOCATION_TRACKER.getOr(id, () => locationAtCallStack(4))
     hitAssertion({ id, location, ...args })
+}
+
+function guidanceImpl<T extends GuidanceType>(
+    args: T extends unknown
+        ? {
+              message: string
+              guidanceType: T
+              maximize: boolean
+              data: GuidanceData[T]
+          }
+        : never
+) {
+    const id = args.message
+    const location = LOCATION_TRACKER.getOr(id, () => locationAtCallStack(4))
+    hitGuidance({ id, location, ...args })
 }
