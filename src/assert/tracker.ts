a94471bdd61602136d0c65d1b9b5ad86594e68df
Shuxian Wang <shuxian.wang@antithesis.com>
2024-11-17 00:01:42 +0800

    Rework assertions
    - Separate out assertion registration (for cataloging) and assertion runtime reporting.
    - Introduce the tracker abstraction to cache per-assertion (identified by their messages) information, including their source location to prevent unnecessary recalc at runtime.
    - Unit tests for assertions are added, but currently the location reporting is incorrect, which will be fixed in the next commit.

--- src/assert/tracker.ts
+++ src/assert/tracker.ts
@@ -1,65 +1,19 @@
-import { type LocationInfo } from './location'
-import { JSONValue } from '../internal'
-import * as internal from '../internal'
+export class Tracker<K, V> {
+    #tracker: Map<K, V>
 
-export type AssertType = 'every' | 'some' | 'none'
-
-export type AssertInfo = {
-    hit: boolean
-    must_hit: boolean
-    assert_type: AssertType
-    expecting: boolean
-    category: string
-    message: string
-    condition: boolean
-    id: string
-    location: LocationInfo
-    details: JSONValue // Map<string, JSONValue>
-}
-
-type TrackerInfo = {
-    pass_count: number
-    fail_count: number
-}
-
-type MaybeTrackerInfo = TrackerInfo | undefined
-
-const assert_info_tag = 'antithesis_assert'
-
-// assert_tracker (global) keeps track of the unique asserts evaluated
-export const assert_tracker: Map<string, TrackerInfo> = new Map()
-
-export const get_tracker_entry = (message_key: string): MaybeTrackerInfo => {
-    if (!(assert_tracker instanceof Map)) {
-        return undefined
+    constructor() {
+        this.#tracker = new Map()
     }
-    let tracker_entry: MaybeTrackerInfo = assert_tracker.get(message_key)
-    if (!tracker_entry) {
-        tracker_entry = {
-            pass_count: 0,
-            fail_count: 0,
-        }
-        assert_tracker.set(message_key, tracker_entry)
-    }
-    return tracker_entry
-}
 
-export const emit_assertion = (ti: TrackerInfo, ai: AssertInfo) => {
-    const cond = ai.condition
-    if (cond) {
-        if (ti.pass_count === 0) {
-            do_emit(ai)
-        }
-        ti.pass_count++
-        return
+    set(key: K, val: V) {
+        this.#tracker.set(key, val)
     }
-    if (ti.fail_count === 0) {
-        do_emit(ai)
-    }
-    ti.fail_count++
-    return
-}
 
-const do_emit = (ai: AssertInfo) => {
-    internal.output({ antithesis_assert: ai })
+    getOr(key: K, init: () => V): V {
+        const result = this.#tracker.get(key)
+        if (result !== undefined) return result
+        const value = init()
+        this.#tracker.set(key, value)
+        return value
+    }
 }
